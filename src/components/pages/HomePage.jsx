57591171d834626c5a028d45b1dab8103a7d5295
dandybytes <dandybytes@gmx.com>
2019-11-14 05:47:41 +0200

    create HomePage component; style adjustments

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import "./HomePage.scss";
 
 const HomePage = () => {
     return (
         <div className="home-page">
-            <h1>Home Page</h1>
+            <section className="home-poster">
+                <div className="home-poster-message">
+                    <h1 className="home-poster-title">A new perspective on the world...</h1>
+                    <Link to="/products" className="link-button">
+                        shop now
+                    </Link>
+                </div>
+            </section>
         </div>
     );
 };
