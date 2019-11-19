e5e5212b7ca8877af25ad037299568ffb3db91cb
dandybytes <dandybytes@gmx.com>
2019-11-19 10:13:46 +0200

    make LinkButton take children props for content; remove default margin for all icons from index.scss

--- src/components/common/LinkButton.jsx
+++ src/components/common/LinkButton.jsx
@@ -4,21 +4,22 @@ import PropTypes from "prop-types";
 
 import "./LinkButton.scss";
 
-const LinkButton = ({destination, className, message, onClick, inverted}) => {
+const LinkButton = ({destination, className, message, children, onClick, inverted}) => {
     return (
         <Link
             to={destination}
             className={`${className}${inverted ? " inverted" : " regular"}`}
             onClick={onClick}
         >
-            {message}
+            {message && message}
+            {children && children}
         </Link>
     );
 };
 
 LinkButton.propTypes = {
     destination: PropTypes.string.isRequired,
-    message: PropTypes.string.isRequired,
+    message: PropTypes.string,
     className: PropTypes.string
 };
 
