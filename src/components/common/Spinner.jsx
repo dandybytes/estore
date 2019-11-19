6019d9af818c3ab0c09aa0a522a45a73d3b2f48b
dandybytes <dandybytes@gmx.com>
2019-11-19 09:50:05 +0200

    create Spinner component

---
+++ src/components/common/Spinner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Spinner.scss";
+
+const Spinner = () => (
+    <div className="spinner">
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
+
+export default Spinner;
