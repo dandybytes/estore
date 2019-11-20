e3604d05dc820521873b7af2e488b25d48a98e97
dandybytes <dandybytes@gmx.com>
2019-11-20 17:38:29 +0200

    create NotFoundPage

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Title from "../common/Title";
+import LinkButton from "../common/LinkButton";
+import GoBackIcon from "../icons/GoBackIcon";
+import "./NotFoundPage.scss";
 
 const NotFoundPage = () => {
     return (
         <div className="notfound-page">
-            <h1>NotFound Page</h1>
+            <Title text="We couldn't find the page you're looking for." center></Title>
+            <LinkButton destination="/" inverted>
+                <GoBackIcon size={15} fill="white" />
+                <span>Home Page</span>
+            </LinkButton>
         </div>
     );
 };
